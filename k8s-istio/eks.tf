03e817c42546e40916fb57ac7e391bd80be54f33
manoj <manoj>
2024-05-04 11:51:34 +0530

    istio terraform code is ready

---
+++ k8s-istio/eks.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_role" "eks" {
+  name = "eks-cluster-Role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks_cluster_dev"
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    subnet_ids = concat(module.vpc.private-subnet, module.vpc.public-subnet)
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy,
+  aws_iam_role_policy_attachment.eks-AmazonEKSVPCResourceController]
+}
